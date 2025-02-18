fd4d0967b6a32b0eb498e6af39db1fdb3d84d3f1
“[Nati]” <[nna77803@gmail.com]>
2025-02-18 23:57:29 +0300

    Completed Sidebar styles

--- src/state/api.ts
+++ src/state/api.ts
@@ -1,9 +1,9 @@
-import {  createApi, fetchbaseQuery } from "@reduxjs/toolkit/query";
+import {  createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
 import build from "next/dist/build";
 import { buffer } from "stream/consumers";
 
 export const api = createApi({
-    baseQuery: fetchbaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL}),
+    baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL}),
     reducerPath: "api",
     tagTypes: [],
     endpoints: (build) => ({}),
